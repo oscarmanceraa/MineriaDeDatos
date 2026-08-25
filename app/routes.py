eceaa2e30cf464a26c56c9af21b613f58c455804
Oscar Mancera <oscarmancera20@gmail.com>
2026-08-24 20:37:55 -0500

    plantilla inicial

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,8 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/etapa-1')
+def etapa1():
+    return render_template('etapa1.html')
